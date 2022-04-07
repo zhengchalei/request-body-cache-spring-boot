1955827ab5ed07c120b62c0928fef643910b1022
xiaoshitou <stone981023@gmail.com>
2022-04-08 01:03:16 +0800

    First Commit

---
+++ src/main/java/com/github/zhengchalei/config/BodyCacheFilter.java
@@ -0,0 +1,26 @@
+package com.github.zhengchalei.config;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 用户转换 Request
+ *
+ * @author 郑查磊
+ * @date 2022年4月8日00:30:19
+ */
+@Order(Integer.MIN_VALUE)
+@Component
+public class BodyCacheFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        BodyCacheHttpServletRequestWrapper bladeRequest = new BodyCacheHttpServletRequestWrapper((HttpServletRequest) servletRequest);
+        filterChain.doFilter(bladeRequest, servletResponse);
+    }
+
+}
---
+++ src/main/java/com/github/zhengchalei/config/BodyCacheHttpServletRequestWrapper.java
@@ -0,0 +1,67 @@
+package com.github.zhengchalei.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * BodyCacheHttpServletRequestWrapper
+ *
+ * @author 郑查磊
+ * @date 2022年4月8日00:30:27
+ */
+@Primary
+@Configuration
+public class BodyCacheHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+    private byte[] bodyBytes = null;
+
+    public BodyCacheHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        // 第一次缓存
+        if (bodyBytes == null) bodyBytes = getBytes(super.getInputStream());
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bodyBytes);
+        return new ServletInputStream() {
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+
+            }
+
+            @Override
+            public int read() {
+                return byteArrayInputStream.read();
+            }
+        };
+    }
+
+    private byte[] getBytes(InputStream in) throws IOException {
+        ByteArrayOutputStream byteOs = new ByteArrayOutputStream();
+        int data;
+        while ((data = in.read()) != -1) {
+            byteOs.write(data);
+        }
+        return byteOs.toByteArray();
+    }
+}
